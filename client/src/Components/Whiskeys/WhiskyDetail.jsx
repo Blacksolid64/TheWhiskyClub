bc937b79621212828b1474239a0a80fb222f3380
JosueNavarroSolis <56096921+JosueNavarroSolis@users.noreply.github.com>
2022-06-11 21:26:21 -0600

    whisky Detail

--- client/src/Components/Whiskeys/WhiskyDetail.jsx
+++ client/src/Components/Whiskeys/WhiskyDetail.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom'
 
 export function WhiskyDetail() {
 
-    const {state} = useLocation();
-    const whisky = state.whisky
+    
+    const whisky = "state.whisky"
 
   return (
     <Fragment>
         <header className="App-header">
             <div style={{ backgroundImage: 'url(require("./images/genericBackground.png"))' }}>
                 <div className='container mx-auto'>
-                    <div className="card bg-light w-100 mb-3" >                    
+                    <div className="card bg-dark w-100 mb-3" >                    
                         <div className="row g-0">
                             <div className="col-md-4">
                             <img src={whisky.image} className="img-fluid rounded-start" alt="..."/>
@@ -25,12 +25,12 @@ export function WhiskyDetail() {
                                     <div className="row">
                                         <div className="col">
                                             <label htmlFor="text" className="form-label">Type</label>
-                                            <input type="text" className="form-control" placeholder="Typewhisky" aria-label="Typewhisky" value = {whisky.type} readOnly/>
+                                            <input type="text" className="form-control" placeholder="Type Whisky" aria-label="Type Whisky" value = {whisky.type} readOnly/>
                                         </div>
                                         
                                         <div className="col">
                                             <label htmlFor="text" className="form-label">Aged</label>
-                                            <textarea type="text" style={{resize:'none'}} className="form-control" placeholder="Agedwhisky" aria-label="Agedwhisky" value = {whisky.location} readOnly/>
+                                            <textarea type="text" style={{resize:'none'}} className="form-control" placeholder="Aged Whisky" aria-label="Aged Whisky" value = {whisky.aged} readOnly/>
                                         </div>
 
                                     </div>
@@ -39,7 +39,7 @@ export function WhiskyDetail() {
                                     <div className="row">
                                         <div className="col">
                                             <label htmlFor="text" className="form-label">Distillery</label>
-                                            <input type="text" className="form-control" placeholder="distillery" aria-label="Distillery" value = {whisky.supplier} readOnly/>
+                                            <input type="text" className="form-control" placeholder="Distillery" aria-label="Distillery" value = {whisky.supplier} readOnly/>
                                         </div>
                                         <div className="col">
                                             <label htmlFor="text" className="form-label">Presentation</label>
@@ -68,10 +68,17 @@ export function WhiskyDetail() {
 
                                     </div>
 
+                                    <div>
+                                        <div className="col">
+                                            <label htmlFor="text" className="form-label">Amount to add to cart</label>
+                                            <input type="text" className="form-control" placeholder="Amount to add to cart" aria-label="Cart" value = {whisky.store}/>
+                                        </div>
+                                    </div>
+
                                     <br></br>
 
                                     <center>
-                                        <Link to= '/ManageParking' className="btn btn-danger">Regresar</Link>    
+                                        <Link to= '/ManageParking' className="btn btn-light">Regresar</Link>    
                                     </center>
                                     
                                 </div>
@@ -85,3 +92,4 @@ export function WhiskyDetail() {
     
   )
 }
+//const {state} = useLocation();
