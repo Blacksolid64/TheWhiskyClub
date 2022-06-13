caa737a8802390f7115182aa0258f4bc058d677b
jacob0532 <61889488+jacob0532@users.noreply.github.com>
2022-06-13 16:25:10 -0600

    Filter Frontend

--- client/src/Components/Whiskeys/ReadWhiskeys.jsx
+++ client/src/Components/Whiskeys/ReadWhiskeys.jsx
@@ -1,10 +1,20 @@
 import React, { Fragment, useState, useEffect }from 'react'
-import { Table,Button } from 'reactstrap';
+import { Table,Button,Dropdown,DropdownMenu,DropdownToggle,DropdownItem,Input} from 'reactstrap';
 import axios from 'axios';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export  function ReadWhiskeys() {
     const [whiskys, setwhiskys] = useState([]);
+    const [dropdownOpen1,setDropdownOpen1] = useState(false);
+    const [dropdownOpen2,setDropdownOpen2] = useState(false);
+    const [dropdownOpen3,setDropdownOpen3] = useState(false);
+    const [dropdownOpen4,setDropdownOpen4] = useState(false);
+    const toggle1 = () => setDropdownOpen1(prevState => !prevState);
+    const toggle2 = () => setDropdownOpen2(prevState => !prevState);
+    const toggle3 = () => setDropdownOpen3(prevState => !prevState);
+    const toggle4 = () => setDropdownOpen4(prevState => !prevState);
+
+    const [name, setName] = useState('');
     async function getWhiskys(){
     	const response  = await axios.get('http://localhost:3001/whisky/whiskysSelectAll');
     	if(whiskys.length===0)
@@ -14,10 +24,90 @@ export  function ReadWhiskeys() {
     	//Runs only on the first render
 	    getWhiskys()
 	}, []);
+
+	const handleSubmit = (e) => {
+    
+        e.preventDefault();
+
+        console.log(`Form submitted, ${name}`);    
+
+    }
+
+
     return (
     	<Fragment>
             <header className="App-header2" style={{textAlign: 'right'}}>
                 <div style={{ backgroundImage: 'url(require("./images/background.png"))' }}>
+                <div className="d-flex justify-content-center p-5">
+                <Dropdown isOpen={dropdownOpen1} toggle={toggle1}>
+				    <DropdownToggle caret style={{backgroundColor: '#262626'}}>
+				      Any Whiskey type
+				    </DropdownToggle>
+				    <DropdownMenu>
+				      <DropdownItem>
+				        Whiskey type3
+				      </DropdownItem>
+				      <DropdownItem>
+				        Whiskey type2
+				      </DropdownItem>
+				      <DropdownItem>
+				      	Any Whiskey type
+				      </DropdownItem>
+				    </DropdownMenu>
+				  </Dropdown>
+				  <Dropdown isOpen={dropdownOpen2} toggle={toggle2}>
+				    <DropdownToggle caret style={{backgroundColor: '#262626'}}>
+				      Any Price
+				    </DropdownToggle>
+				    <DropdownMenu>
+				      <DropdownItem>
+				        Min Price
+				      </DropdownItem>
+				      <DropdownItem>
+				        Max Price
+				      </DropdownItem>
+				      <DropdownItem>
+				      	Any Price
+				      </DropdownItem>
+				    </DropdownMenu>
+				  </Dropdown>
+				  <Dropdown isOpen={dropdownOpen3} toggle={toggle3}>
+				    <DropdownToggle caret style={{backgroundColor: '#262626'}}>
+				      Any Popular
+				    </DropdownToggle>
+				    <DropdownMenu>
+				      <DropdownItem>
+				        Min Popular
+				      </DropdownItem>
+				      <DropdownItem>
+				        Max Popular
+				      </DropdownItem>
+				      <DropdownItem>
+				      	Any Popular
+				      </DropdownItem>
+				    </DropdownMenu>
+				  </Dropdown>
+				  <Dropdown isOpen={dropdownOpen4} toggle={toggle4}>
+				    <DropdownToggle caret style={{backgroundColor: '#262626'}}>
+				      Any Distance
+				    </DropdownToggle>
+				    <DropdownMenu>
+				      <DropdownItem>
+				        Min Distance
+				      </DropdownItem>
+				      <DropdownItem>
+				        Max Distance
+				      </DropdownItem>
+				      <DropdownItem>
+				      	Any Distance
+				      </DropdownItem>
+				    </DropdownMenu>
+				  </Dropdown>
+                <form onSubmit = {handleSubmit}>
+		            <input onChange = {(e) => setName(e.target.value)} value = {name}></input>
+		            <button type = 'submit' className="btn btn-dark btn-lg">Buscar</button>
+		        </form>
+				</div>
                 <Table striped bordered style={{backgroundColor: '#FFF'}}>
 				  <thead >
 				    <tr>
