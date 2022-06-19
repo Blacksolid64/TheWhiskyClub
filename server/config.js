57bb0049763fcd09d2f03d449dfead2ca471dcc0
Blacksolid64 <48729275+Blacksolid64@users.noreply.github.com>
2022-06-18 20:21:15 -0600

    Added stored procedures for sales

--- server/config.js
+++ server/config.js
@@ -1,11 +1,7 @@
 const sql = require("mssql/msnodesqlv8");
-<<<<<<< Updated upstream
 const connections = []
 
 const conn1 = new sql.ConnectionPool( {
-=======
-const conn = new sql.ConnectionPool( {
->>>>>>> Stashed changes
     server:'LAPTOP-140FDP4P',
     driver: 'msnodesqlv8',
     database:'WC_USA',
@@ -14,7 +10,7 @@ const conn = new sql.ConnectionPool( {
     }
 });
 
-const conn1 = new sql.ConnectionPool( {
+const conn2 = new sql.ConnectionPool( {
     server:'LAPTOP-RHFFFTGS',
     driver: 'msnodesqlv8',
     database:'WC_USA',
@@ -23,7 +19,7 @@ const conn1 = new sql.ConnectionPool( {
     }
 });
 
-const conn2 = new sql.ConnectionPool( {//Joshua
+const conn = new sql.ConnectionPool( {//Joshua
     server:'DESKTOP-U9I8H26\\SQLEXPRESS',
     driver: 'msnodesqlv8',
     database:'WC_USA',
